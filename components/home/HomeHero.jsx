9ce4e6889fad92f0fdd478eabf774ef05bb0df5a
Ameer Hamza <162882050+HamzaRajpu456@users.noreply.github.com>
2024-05-27 22:54:48 +0500

    adds animations to home hero

--- components/home/HomeHero.jsx
+++ components/home/HomeHero.jsx
@@ -1,26 +1,54 @@
+"use client";
 import React from 'react'
 import Wrapper from '../Wrapper'
 import { Button, Image } from '@nextui-org/react'
 import { FaArrowRightFromBracket } from "react-icons/fa6";
 import { TfiHeadphoneAlt } from "react-icons/tfi";
+import {motion} from "framer-motion"
 const HomeHero = () => {
+    const container = (delay)=>(
+        {
+            hidden:{x: -100, opacity:0},
+            visible:{
+                x:0,
+                opacity:1,
+                transition:{duration:0.5,delay: delay}
+            }
+        }
+    )
   return (
    <Wrapper>
    <div className="flex items-center justify-center min-h-screen">
    <div className="md:grid md:grid-cols-2 md:gap-10 space-y-10 md:space-y-0">
    <section className='flex flex-col text-center max-w-md mx-auto md:max-w-full md:text-start justify-center'>
-        <h1 className='tracking-tight mb-5 font-semibold text-3xl sm:text-4xl md:text-5xl text-pretty leading-10'>Streamline Identity <span className='tracking-tight  from-[#FF1CF7] to-[#b249f8] bg-clip-text text-transparent bg-gradient-to-b'>Verification</span> with Facial Recognition</h1>
-        <p className='text-large mb-5'>Embrace safer communities with <span className='underline underline-offset-4'>FaceGuard</span> global face recognition technology, enhancing identity verification for businesses.</p>
-        <div className='space-x-3'>
+        <motion.h1
+     variants={container(0)}
+     initial="hidden"
+     animate="visible"
+        className='tracking-tight mb-5 font-semibold text-3xl sm:text-4xl md:text-5xl text-pretty leading-10'>Streamline Identity <span className='tracking-tight  from-[#FF1CF7] to-[#b249f8] bg-clip-text text-transparent bg-gradient-to-b'>Verification</span> with Facial Recognition</motion.h1>
+        <motion.p
+         variants={container(0.5)}
+         initial="hidden"
+         animate="visible"
+        className='text-large mb-5'>Embrace safer communities with <span className='underline underline-offset-4'>FaceGuard</span> global face recognition technology, enhancing identity verification for businesses.</motion.p>
+        <motion.div
+         variants={container(1)}
+         initial="hidden"
+         animate="visible"
+        className='space-x-3'>
             <Button endContent={<TfiHeadphoneAlt />} color="primary" variant="shadow">
                 Talk to an Expert
             </Button>
             <Button endContent={<FaArrowRightFromBracket />} color="default" variant="bordered">
                 Let's Start
             </Button>
-        </div>
+        </motion.div>
     </section>
-    <div className='flex items-center justify-center'>
+    <motion.div 
+    initial={{x:100, opacity:0}}
+    animate={{x:0, opacity:1}}
+    transition={{duration:1, delay:1.2}}
+    className='flex items-center justify-center'>
         <Image
         src="/face-detection.webp"
         width={500}
@@ -28,7 +56,7 @@ const HomeHero = () => {
         height={500}
         alt="face detection app"
         />
-    </div>
+    </motion.div>
    </div>
    </div>
    </Wrapper>
